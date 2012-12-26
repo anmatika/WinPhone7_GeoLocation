8c6af340453575bbc5ca765f6ad5ffec5566ebbe
anmatika <4ntti.matikainen@gmail.com>
2012-12-26 11:13:00 +0200

    first

---
+++ Location/Helpers.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Device.Location;
+using System.Linq;
+using System.Text;
+using Microsoft.Phone.Reactive;
+
+namespace Location
+{
+   
+        public static class Helpers
+        {
+            public static IObservable <GeoPositionStatusChangedEventArgs> GetStatusChangedEventStream(this GeoCoordinateWatcher watcher)
+            {
+                return Observable.Create<GeoPositionStatusChangedEventArgs>(observer =>
+                {
+                    EventHandler<GeoPositionStatusChangedEventArgs> handler = (s, e) =>
+                    {
+                        observer.OnNext(e);
+                    };
+                    watcher.StatusChanged += handler;
+                    return () => { watcher.StatusChanged -= handler; };
+                }
+                );
+            }
+
+            public static IObservable<GeoPositionChangedEventArgs<GeoCoordinate>> GetPositionChangedEventStream(this GeoCoordinateWatcher watcher)
+            {
+                return Observable.Create<GeoPositionChangedEventArgs<GeoCoordinate>>(observable =>
+                {
+                    EventHandler<GeoPositionChangedEventArgs<GeoCoordinate>> handler = (s, e) =>
+                    {
+                        observable.OnNext(e);
+                    };
+                    watcher.PositionChanged += handler;
+                    return () => { watcher.PositionChanged -= handler; };
+                }
+                );
+            }
+        }
+    
+}
---
+++ Location/MainPage.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Device.Location;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Reactive;
+
+namespace Location
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+
+        GeoCoordinateWatcher gcw; // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnNavigatedFrom(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            base.OnNavigatedFrom(e);
+            if (gcw != null)
+                gcw.Stop(); 
+        }
+
+        protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            if (gcw == null)
+                gcw = new GeoCoordinateWatcher(GeoPositionAccuracy.High);
+
+            gcw.Start();
+
+            map1.Center = new GeoCoordinate(gcw.Position.Location.Latitude, gcw.Position.Location.Longitude);
+            map1.ZoomLevel = 5;
+            map1.ZoomBarVisibility = Visibility.Visible;
+            gcw.PositionChanged += new EventHandler<GeoPositionChangedEventArgs<GeoCoordinate>>(myCoordinateWatcher_PositionChanged);
+        }
+
+        private void myCoordinateWatcher_PositionChanged(object sender, GeoPositionChangedEventArgs<GeoCoordinate> e)
+        {
+            ShowGeoLocation();
+        }
+
+        private void ShowGeoLocation()
+        {
+            textBlock1.Text = "Latitude: " + gcw.Position.Location.Latitude;
+            textBlock1.Text += "\nLongitude" + gcw.Position.Location.Longitude;
+
+            map1.Center = new GeoCoordinate(gcw.Position.Location.Latitude, gcw.Position.Location.Longitude);
+        }
+
+        private void Refresh_Click(object sender, RoutedEventArgs e)
+        {
+            ShowGeoLocation();
+        }
+
+        
+    }
+}
